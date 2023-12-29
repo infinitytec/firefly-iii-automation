8137e6b9eae707ce7722a08e0b1182d3f302ae75
infinitytec <infinitytec@users.noreply.github.com>
2023-12-29 12:04:23 -0600

    Update installlampwithfirefly.sh

--- installlampwithfirefly.sh
+++ installlampwithfirefly.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install web components
-sudo apt install apache2 mysql-common mariadb-server php8.3 php8.3-common php8.3-bcmath php8.3-intl php8.3-curl php8.3-zip php8.3-gd php8.3-xml php8.3-mbstring php8.3-ldap php8.3-mysql php-mysql php8.3-intl php8.3-xml curl -y
+sudo apt install apache2 mysql-common mariadb-server php8.3 php8.3-common php8.3-bcmath php8.3-intl php8.3-curl php8.3-zip php8.3-gd php8.3-xml php8.3-mbstring php8.3-ldap php8.3-mysql php-mysql php8.3-intl php8.3-xml php-cli curl -y
 
 # Check if Firefly III is installed
 if [ -d /var/www/html/firefly-iii ]; then
